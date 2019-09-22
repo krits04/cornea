4e7f798354e35f9cf14653165be9afafa33ab870
krits04 <35338230+krits04@users.noreply.github.com>
2019-09-23 00:25:16 +0530

    Update prediction matrix convolution.r

--- prediction matrix convolution.r
+++ prediction matrix convolution.r
@@ -5,7 +5,7 @@ cc<-cc[-1,-1]
 r<-nrow(cmi)
 c<-ncol(cmi)
 inf<-matrix(0,r,c)
-x<-as.matrix(read.csv(file="prediction_test.csv", header = F))
+x<-as.matrix(read.csv(file="test_ final_prediction.csv", header = F))
 m<-nrow(x)
 for(i in 1:m)
 {
@@ -15,4 +15,4 @@ y<-matrix(1,3,3)  ##change to matrix(1,5,5) if using 5*5 convolution##
 Z<-convolution(inf, y, mode= "same")
 z.m<-melt(z)
 z.m.f<-z.m[(z.m$value>2),] ##change to z.m$value>6 if using 5*5 matrix##
-write.csv(z.m.f, file="test_final_prediction.csv")
+write.csv(z.m.f, file="final_prediction_test.csv")
